bcd812bf3ff1dd0113aefcc6b5519da13f6d0bf7
Aabhushan gautam <carefreeav09@gmail.com>
2019-09-17 00:32:12 +0545

    Carousel figured out

---
+++ src/components/Home/Locations.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Locations = () => {
+    return (
+        <div>
+            <section className="my-5">
+
+            <div className="row">
+
+                <div className="col-lg-5">
+
+                    <div className="view overlay rounded z-depth-2 mb-lg-0 mb-4">
+                        <img className="img-fluid" src="https://d3hne3c382ip58.cloudfront.net/resized/750x420/kathmandu-valley-trek-tour-2-269861_1559744945.JPG" alt="Sample image" />
+                            <a>
+                                <div className="mask rgba-white-slight" />
+                            </a>
+                    </div>
+
+                </div>
+
+                <div className="col-lg-7 white-text">
+
+                    <a href="#!" className="green-text">
+                        <h6 className="font-weight-bold mb-3"><i className="fas fa-city pr-2" />City of Temples</h6>
+                    </a>
+                    <h3 className="font-weight-bold mb-3"><strong>Kathmandu</strong></h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus enim eum harum quae tenetur, ullam? Ab adipisci aut culpa earum perspiciatis porro quis quisquam reiciendis velit veritatis, voluptas voluptates?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi, commodi doloribus magnam magni modi placeat quia quidem sint ut! Asperiores consectetur dolores, eos itaque magni omnis quisquam reprehenderit voluptatum!
+                        <br/>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae dignissimos exercitationem facere facilis harum ipsam laborum nobis odit perferendis perspiciatis qui quisquam reiciendis, sapiente sequi sunt totam veniam voluptatum!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consectetur, dolor ducimus eius et ipsa iusto, nihil odio quos reiciendis sapiente sequi ullam, vero. Aliquid delectus et molestias optio quis!
+                        <br/>
+                    </p>
+
+                </div>
+
+            </div>
+                </section>
+        </div>
+    );
+};
+
+export default Locations;
